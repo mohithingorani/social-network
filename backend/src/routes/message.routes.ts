71edce2976c4e8efa926c9ad489dfba1ba73c7a8
Mohit Hingorani <Mohithingorani2003@gmail.com>
2026-05-10 06:28:15 +0530

    update routes

--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -1,8 +1,15 @@
 import Router from "express";
-import { createMessage, getAllMessages } from "../controllers/message.controller";
+import {
+  createMessage,
+  getAllMessages,
+  getInbox,
+  markRead,
+} from "../controllers/message.controller";
 const router = Router();
 
 router.post("/create", createMessage);
 router.get("/allMessages", getAllMessages);
+router.get("/inbox", getInbox);
+router.post("/markRead", markRead);
 
 export default router;
--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -1,5 +1,5 @@
 import Router from "express";
-import { deletePost, generateUploadUrl, getPosts, likePost, postCount, unlikePost, uploadPost, uploadWithoutImage } from "../controllers/post.controller";
+import { createPost, deletePost, generateUploadUrl, getPosts, likePost, postCount, unlikePost, uploadPost, uploadWithoutImage } from "../controllers/post.controller";
 const router = Router();
 
 router.get("/getPosts",getPosts);
@@ -10,5 +10,6 @@ router.post("/unlikePost", unlikePost);
 router.post("/uploadPost",uploadPost);
 router.post("/uploadWithoutImage",uploadWithoutImage);
 router.post("/generateUploadUrl",generateUploadUrl);
+router.post("/create",createPost);
 
 export default router;
